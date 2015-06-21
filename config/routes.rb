8b6639b2480f33144d9a3810aa06a9f96b2098e5
角田 均 <tsunoda@aomori-u.ac.jp>
2015-06-21 23:17:46 +0900

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 MapApp::Application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 
   root  'static_pages#home'
   match '/signup',  to: 'users#new',            via: 'get'
+  match '/signin',  to: 'sessions#new',         via: 'get'
+  match '/signout', to: 'sessions#destroy',     via: 'delete'
   match '/about',   to: 'static_pages#about',   via: 'get'
 end
